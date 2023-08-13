a8f678c5489e77cbc52042ddb743572ec74340fe
Touka <gustavoluis.pixell@gmail.com>
2023-08-13 20:21:33 -0300

    feat: add user and access creation route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,13 @@
+import 'express-async-errors'
 import express from 'express'
+import userRouter from './routes/User.routes'
+import accessRouter from './routes/Access.routes'
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
+
+app.use('/register', userRouter)
+app.use('/access', accessRouter)
 
 export default app
